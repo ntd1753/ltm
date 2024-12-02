b4498545c2e6a3aa308ff6405d7b1bd2ed1413f2
Rang612 <tranhuonggiang6122003@gmail.com>
2024-12-02 10:48:47 +0700

    task_management

--- build/CMakeFiles/json_test.dir/DependInfo.cmake
+++ build/CMakeFiles/json_test.dir/DependInfo.cmake
@@ -8,19 +8,16 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/vboxuser/ltm/src/file_utils.c" "CMakeFiles/json_test.dir/file_utils.c.o" "gcc" "CMakeFiles/json_test.dir/file_utils.c.o.d"
-  "/home/vboxuser/ltm/cJSON/cJSON.c" "CMakeFiles/json_test.dir/home/vboxuser/ltm/cJSON/cJSON.c.o" "gcc" "CMakeFiles/json_test.dir/home/vboxuser/ltm/cJSON/cJSON.c.o.d"
-  "/home/vboxuser/ltm/src/main.c" "CMakeFiles/json_test.dir/main.c.o" "gcc" "CMakeFiles/json_test.dir/main.c.o.d"
-  "/home/vboxuser/ltm/src/project_management.c" "CMakeFiles/json_test.dir/project_management.c.o" "gcc" "CMakeFiles/json_test.dir/project_management.c.o.d"
-  "/home/vboxuser/ltm/src/user_management.c" "CMakeFiles/json_test.dir/user_management.c.o" "gcc" "CMakeFiles/json_test.dir/user_management.c.o.d"
+  "/home/rang/ltm/src/file_utils.c" "CMakeFiles/json_test.dir/file_utils.c.o" "gcc" "CMakeFiles/json_test.dir/file_utils.c.o.d"
+  "/home/rang/ltm/cJSON/cJSON.c" "CMakeFiles/json_test.dir/home/rang/ltm/cJSON/cJSON.c.o" "gcc" "CMakeFiles/json_test.dir/home/rang/ltm/cJSON/cJSON.c.o.d"
+  "/home/rang/ltm/src/main.c" "CMakeFiles/json_test.dir/main.c.o" "gcc" "CMakeFiles/json_test.dir/main.c.o.d"
+  "/home/rang/ltm/src/project_management.c" "CMakeFiles/json_test.dir/project_management.c.o" "gcc" "CMakeFiles/json_test.dir/project_management.c.o.d"
+  "/home/rang/ltm/src/task_management.c" "CMakeFiles/json_test.dir/task_management.c.o" "gcc" "CMakeFiles/json_test.dir/task_management.c.o.d"
+  "/home/rang/ltm/src/user_management.c" "CMakeFiles/json_test.dir/user_management.c.o" "gcc" "CMakeFiles/json_test.dir/user_management.c.o.d"
   )
 
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
-  )
-
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- build/CMakeFiles/json_test.dir/main.c.o.d
+++ build/CMakeFiles/json_test.dir/main.c.o.d
@@ -1,4 +1,4 @@
-CMakeFiles/json_test.dir/main.c.o: /home/vboxuser/ltm/src/main.c \
+CMakeFiles/json_test.dir/main.c.o: /home/rang/ltm/src/main.c \
  /usr/include/stdc-predef.h /usr/include/stdio.h \
  /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
  /usr/include/features.h /usr/include/features-time64.h \
@@ -8,8 +8,8 @@ CMakeFiles/json_test.dir/main.c.o: /home/vboxuser/ltm/src/main.c \
  /usr/include/x86_64-linux-gnu/bits/long-double.h \
  /usr/include/x86_64-linux-gnu/gnu/stubs.h \
  /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stdarg.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stddef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h \
  /usr/include/x86_64-linux-gnu/bits/types.h \
  /usr/include/x86_64-linux-gnu/bits/typesizes.h \
  /usr/include/x86_64-linux-gnu/bits/time64.h \
@@ -19,10 +19,8 @@ CMakeFiles/json_test.dir/main.c.o: /home/vboxuser/ltm/src/main.c \
  /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
  /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
  /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
- /usr/include/x86_64-linux-gnu/bits/types/cookie_io_functions_t.h \
  /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
  /usr/include/x86_64-linux-gnu/bits/floatn.h \
  /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
- /home/vboxuser/ltm/src/user_management.h \
- /home/vboxuser/ltm/src/../cJSON/cJSON.h \
- /home/vboxuser/ltm/src/project_management.h
+ /home/rang/ltm/src/user_management.h /home/rang/ltm/src/../cJSON/cJSON.h \
+ /home/rang/ltm/src/project_management.h
